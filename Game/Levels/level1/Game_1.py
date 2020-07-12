fd1f266bdd8ebafbc2eaaaf34499819c55986fc1
stepaniya <>
2020-07-12 16:35:34 +0300

    2.03

--- Game/Levels/level1/Game_1.py
+++ Game/Levels/level1/Game_1.py
@@ -1,52 +1,44 @@
 import random
-import os
 
 
-while True:
-
-    bot_number_hard = random.randint(1, 100)
-    bot_number_normal = random.randint(1, 50)
-    bot_number_eazy = random.randint(1, 25)
-    chislo_popitoc = 0
-
-
-    def random_number(chislo_popitoc):
-        if slochnost == 1:
-            bot_number = bot_number_eazy
-        if slochnost == 2:
-            bot_number = bot_number_hard
-        if slochnost == 3:
-            bot_number = bot_number_hard
+def random_number():
+    while True:
+        popitci = 10
+        game_popitci = 0
+        bot_number = random.randint(1, 25)
         while True:
             game_number = input('Введи число:')
-            if int(game_number) > bot_number:
-                print('Твоё число больше чем загадоное:')
-                chislo_popitoc += 1
-                print('Число попыток:', chislo_popitoc)
+            if game_number.isdigit():
+                if int(game_number) >= 1 and int(game_number) <= 25:
+                    if int(game_number) > bot_number:
+                        print('Твоё число больше чем загадоное ')
+                        game_popitci += 1
+                        print('Число попыток:', game_popitci)
 
-            elif int(game_number) < bot_number:
-                print('Твоё число менше чем загадоное:')
-                chislo_popitoc += 1
-                print('Число попыток:', chislo_popitoc)
+                    elif int(game_number) < bot_number:
+                        print('Твоё число менше чем загадоное ')
+                        game_popitci += 1
+                        print('Число попыток:', game_popitci)
 
-            elif int(game_number) == bot_number:
-                print('Ты выиграл ')
-                print('Число попыток:', chislo_popitoc)
-                break
+                    elif int(game_number) == bot_number:
+                        print('Ты выиграл ')
+                        print('Число попыток:', game_popitci)
+                        game_popitci = 0
+                        bot_number = random.randint(1, 25)
+                        continue
 
-            if chislo_popitoc == popitci:
-                print('Ты проиграл')
-                print('Число попыток:', chislo_popitoc)
-                break
+                    if game_popitci == popitci:
+                        print('Ты проиграл ')
+                        print('Число бота:', bot_number)
+                        print('Число попыток:', game_popitci)
+                        game_popitci = 0
+                        bot_number = random.randint(1, 25)
+                        continue
+                else:
+                    print('Что ты ввел')
+            else:
+                print('Что ты ввел')
 
 
-    slochnost = int(input('Введи сложность игры 1(1,25) / 2(1,50) / 3(1,100): '))
-    popitci = int(input('Число попыток:'))
-    os.system('cls')
-    print(random_number(chislo_popitoc))
-    igra = input('Хочешь сыграть ещё + / если не то - :')
-    if igra == '+':
-        os.system('cls')
-        continue
-    if igra == '-':
-        break
+
+print(random_number())
