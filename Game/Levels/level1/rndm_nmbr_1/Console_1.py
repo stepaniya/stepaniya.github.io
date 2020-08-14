87c478075347ed74fa8a48fddf87460cf98ffd98
stepaniya <>
2020-08-14 11:54:23 +0300

    9.2

--- Game/Levels/level1/rndm_nmbr_1/Console_1.py
+++
@@ -1,5 +0,0 @@
-Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Home/Desktop/Уроки/Fixed/stepaniya.github.io/Game/Levels/level1/rndm_nmbr_1/Game_1.py
-Введи число:
